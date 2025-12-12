a4b826cf809b666d16dd8ad024e04754ca2863d7
3A18 <3a18@3A18.local>
2025-12-12 12:17:13 +0900

    123123

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 set -o errexit
 
-pip install -r requirements.text
+pip install -r requirements.txt
 
 python manage.py migrate
+
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput || echo "Superuser already exists."
+fi
